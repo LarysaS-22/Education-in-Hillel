87d23083f4fe9c7cdd9c648f0c1d260f9ff461d3
LarysaS-22 <sapega017@gmail.com>
2022-10-06 19:51:41 +0300

    add Lesson01

---
+++ Lesson01/index.js
@@ -0,0 +1,18 @@
+console.log("Hello, World!")
+
+/**
+ * 
+ * 
+ *
+ * 
+ */
+
+
+ alert ("Hello, alert!");
+
+ inputUser = prompt ("Enter your name: ", "");
+
+ a = confirm ("Hello, confirm! Question?");
+ 
+ var a;
+ document.write(a, inputUser);
