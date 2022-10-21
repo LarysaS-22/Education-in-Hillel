daeab4b2704e9e95703b02afc616f84474d939cf
LarysaS-22 <sapega017@gmail.com>
2022-10-21 12:08:16 +0300

    homework 14 corrected

--- Lesson06/scripts/hw14.js
+++ Lesson06/scripts/hw14.js
@@ -1,20 +1,22 @@
 console.log("Hello, World!");
 
 //Реалізуйте функцію generateKey(length, characters), яка повертає рядок 
-//випадкових символів із набору characters довжиною length. span>
+//випадкових символів із набору characters довжиною length. 
 
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 const key = generateKey(16, characters);
 console.log(key); 
 
 function generateKey(lengthRandomArrey, string){
-   const randomArrey=[];
-   const stringToArrey = string.split('');
+   //const randomArrey=[];
+   //const stringToArrey = string.split('');
+   let  stringRandom="";
    
    for(let i = 0; i < lengthRandomArrey ; i++ ) {
+    stringRandom = stringRandom + string.charAt(Math.random() * string.length);
 
-    randomArrey.push(stringToArrey[Math.floor(Math.random() * stringToArrey.length)]);
-    console.log(randomArrey);
+    //randomArrey.push(]);
+    console.log(stringRandom);
    }
-   return (randomArrey.join(''));
+   return stringRandom;
 };
